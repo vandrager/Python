c5325eae5f99405f0208e80f5d7f768d7b41f65e
vandrager <vandrager@naver.com>
2021-01-08 11:57:11 +0900

    210108_1

--- Study/ACA_python.py
+++ Study/ACA_python.py
@@ -1,60 +1,60 @@
-menu = ['아메리카노', '카페라떼', '카라멜마끼아또', '카페모카', '프라푸치노']
-price = [3000, 5400, 3200, 4000, 6000]
+# menu = ['아메리카노', '카페라떼', '카라멜마끼아또', '카페모카', '프라푸치노']
+# price = [3000, 5400, 3200, 4000, 6000]
 
-ame_menu = menu.index('아메리카노')
-ame_price = price[ame_menu]
-print(ame_price)
+# ame_menu = menu.index('아메리카노')
+# ame_price = price[ame_menu]
+# print(ame_price)
 
-a = list(range(1, 10))
-print(a)
+# a = list(range(1, 10))
+# print(a)
 
-student_info = {'name': "kale", "age": 20, "height": 180, "weight": 65}
-print(student_info.items())
-print(student_info.keys())
-print(student_info.values())
-print("="*90)
-for (k, v) in student_info.items():
-    print("* {}: {}".format(k, v), end="\n")
+# student_info = {'name': "kale", "age": 20, "height": 180, "weight": 65}
+# print(student_info.items())
+# print(student_info.keys())
+# print(student_info.values())
+# print("="*90)
+# for (k, v) in student_info.items():
+#     print("* {}: {}".format(k, v), end="\n")
 
-import time
+# import time
 import os
-os.chdir('C://Users//vandr//OneDrive//바탕 화면//Bigdata//Python//Study')
-print(time.time())
-print("2초간 정적이 흘렀다.", time.sleep(0.5))
+# os.chdir('C://Users//vandr//OneDrive//바탕 화면//Bigdata//Python//Study')
+# print(time.time())
+# print("2초간 정적이 흘렀다.", time.sleep(0.5))
 
-data = open('data.txt', 'r', encoding='utf-8')
-datal = data.read()
-print(datal)
+# data = open('data.txt', 'r', encoding='utf-8')
+# datal = data.read()
+# print(datal)
 
-dateline = data.readline()
-print(dateline)
+# dateline = data.readline()
+# print(dateline)
 
-user_input = input("> 내용을 입력하시오: ")
-datafile = open('data.txt', 'w', encoding='utf-8')
-datafile.write(user_input + '\n')
-datafile = open('data.txt', 'a', encoding='utf-8')
-datafile.write(user_input + '\n')
-datafile = open('data.txt', 'r', encoding='utf-8')
-line = 'init'
-while line:
-    line = datafile.readline()
-    print(line.strip())
+# user_input = input("> 내용을 입력하시오: ")
+# datafile = open('data.txt', 'w', encoding='utf-8')
+# datafile.write(user_input + '\n')
+# datafile = open('data.txt', 'a', encoding='utf-8')
+# datafile.write(user_input + '\n')
+# datafile = open('data.txt', 'r', encoding='utf-8')
+# line = 'init'
+# while line:
+#     line = datafile.readline()
+#     print(line.strip())
 os.chdir("C:/Users/vandr/OneDrive/바탕 화면/Bigdata/Data/practice")
-import csv
-f = open('전국마을기업표준데이터.csv', 'rt')
-csvread = csv.reader(f)
-print(csvread)
+# import csv
+# f = open('전국마을기업표준데이터.csv', 'rt')
+# csvread = csv.reader(f)
+# print(csvread)
 
-import pandas as pd
+import pandas
 import os
 import openpyxl
 
 print(os.getcwd())
 
-parking = pd.read_csv('전국마을기업표준데이터.csv', header=0, encoding= 'euc-kr')
+parking = pandas.read_csv('전국마을기업표준데이터.csv', header=0, encoding= 'euc-kr')
 print(parking.head(3))
 
-park_2 = pd.read_excel('선별진료소_20201226152643.xlsx')
+park_2 = pandas.read_excel('선별진료소_20201226152643.xlsx')
 print(park_2.head(3))
 
 parking.to_csv('park.csv')
--- Study/Machine Learning/01_ready.py
+++ Study/Machine Learning/01_ready.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import csv, usecsv, os, re
 dict_data = {'a': 1, "b": 2, "c": 3}
-k = pd.Series(dict_data)
+k = pd.Series(dict_data) # 시리즈는 1:1대응
 print(k)
 list_data = [10, 100, 1000, 10000]
 k = pd.Series(list_data, index=["동전", "동전2", "지폐", "지폐2"])
@@ -15,8 +15,8 @@ data = pd.read_csv("아파트(매매)__실거래가_20210105153210.csv", encodin
 print(data.head(5))
 
 d = {"name": ['henry', 'shine', 'ryan'],
-"height": [171, 180, 165],
-"weight": [80, 90, 100]}
+    "height": [171, 180, 165],
+    "weight": [80, 90, 100]}
 
 df = pd.DataFrame(d)
 print(df)
@@ -27,7 +27,7 @@ df2 = pd.DataFrame([['영어', '역북초', 390],
                     index=['진솔', '영화', '휘재'],
                     columns=["과목", "출신초교", "수능점수"])
 
-# 판다스 열이름, 인덱스 변경하는 법 inplace=True를 옆에 넣어줘야 바뀐다 임마!
+# 판다스 열이름, 인덱스 변경하는 법 + inplace=True를 옆에 넣어줘야 바뀐다 임마!
 df2.rename(columns={"과목": "자신있는 과목", "출신초교": "출신학교", "수능점수": "받아쓰기점수"}, inplace=True)
 df2.rename(index={"진솔": "학생1", "영화": "학생2", "휘재": "학생3"}, inplace=True)
 print(df2)
@@ -44,5 +44,205 @@ print(df2)
 # 학생1      영어  역북초
 # 학생2      수학  신갈초
 
+df2.rename(columns={"자신있는 과목": "과목"}, inplace=True)
+df2.rename(index={"학생1": "수원"}, inplace=True)
+print(df2)
+#      과목 출신학교
+# 수원   영어  역북초
+# 학생2  수학  신갈초
 
+df = pd.DataFrame([[90, 80, 70], [98, 89, 95], [85, 74, 25], [94, 25, 85]],
+                    columns=['수학', '영어', '도덕'],
+                    index=['민지', '설아', '수지', '초아'])
+print(df)
 
+#행만 선택해서 출력하기
+label1 = df.loc['민지']
+position1 = df.iloc[0]
+print(label1, '\n')
+print(position1)
+
+label2 = df.loc[['민지', '설아']]
+print(label2)
+position2 = df.iloc[[0, 1]]
+print(position2)
+print(df.iloc[::2])
+print(df.iloc[::-1]) #역순으로 출력하기
+#     수학  영어  도덕
+# 민지  90  80  70
+# 설아  98  89  95
+#     수학  영어  도덕
+# 민지  90  80  70
+# 설아  98  89  95
+#     수학  영어  도덕
+# 민지  90  80  70
+# 수지  85  74  25
+#     수학  영어  도덕
+# 초아  94  25  85
+# 수지  85  74  25
+# 설아  98  89  95
+# 민지  90  80  70
+
+#열만 선택해서 출력하기
+print(df['수학'])
+print(df.도덕)
+# 민지    90
+# 설아    98
+# 수지    85
+# 초아    94
+# Name: 수학, dtype: int64
+# 민지    70
+# 설아    95
+# 수지    25
+# 초아    85
+# Name: 도덕, dtype: int64
+
+df = pd.DataFrame([['민지', 90, 80, 70, 80], ['설아', 98, 89, 95, 90], ['수지', 85, 74, 25, 79], ['초아', 94, 25, 85, 88]],
+                    columns=['이름', '수학', '영어', '도덕', '역사'])
+
+df.set_index('이름', inplace=True)
+print(df)
+
+#     수학  영어  도덕
+# 이름
+# 민지  90  80  70
+# 설아  98  89  95
+# 수지  85  74  25
+# 초아  94  25  85
+
+label1 = df.loc['민지', ['영어', '도덕']]
+position1 = df.iloc[0, [1, 2]]
+print(label1, '\n')
+print(position1)
+# 영어    80
+# 도덕    70
+# Name: 민지, dtype: int64
+
+# 영어    80
+# 도덕    70
+# Name: 민지, dtype: int64
+
+#아래와 같이 슬라이싱도 가능
+label1 = df.loc['민지', '도덕':'역사']
+position1 = df.iloc[0, 2:4]
+print(label1, '\n')
+print(position1)
+# 도덕    70
+# 역사    80
+# Name: 민지, dtype: int64
+
+# 도덕    70
+# 역사    80
+# Name: 민지, dtype: int64
+
+label1 = df.loc['민지', '영어':]
+print(label1, '\n')
+# 영어    80
+# 도덕    70
+# 역사    80
+# Name: 민지, dtype: int64
+
+position1 = df.iloc[0, 1:]
+print(position1)
+# 영어    80
+# 도덕    70
+# 역사    80
+# Name: 민지, dtype: int64
+
+label1 = df.loc[['민지', '초아'], '영어':]
+print(label1, '\n')
+
+position1 = df.iloc[[0, 3], 1:]
+print(position1)
+#    영어  도덕  역사
+# 이름
+# 민지  80  70  80
+# 초아  25  85  88
+
+#     영어  도덕  역사
+# 이름
+# 민지  80  70  80
+# 초아  25  85  88
+
+print(df)
+
+df['과학'] = [90, 80, 98, 78]
+print(df)
+df.loc['재호'] = [ 20, 30, 40, 73, 92]
+df.loc['코조'] = 0
+df.loc['야로'] = df.loc['코조']
+print(df)
+#     수학  영어  도덕  역사  과학
+# 이름
+# 민지  90  80  70  80  90
+# 설아  98  89  95  90  80
+# 수지  85  74  25  79  98
+# 초아  94  25  85  88  78
+# 재호  20  30  40  73  92
+# 코조   0   0   0   0   0
+# 야로   0   0   0   0   0
+df.loc['코조':'야로', 0:] = [[80, 90, 70, 50, 30], [90, 20, 40, 90, 80]]
+print(df)
+# 행열바꾸기
+# df.transpose() or df.T
+print(type(df))
+df.transpose()
+print(df.transpose())
+
+# 인덱스 활용
+# 행 인데스 재배열
+new_name = ['설아', '재호', '수지', '초아', '용식', '근정']
+ndf = df.reindex(new_name, fill_value = 0)
+#       수학    영어    도덕    역사    과학
+# 이름
+# 설아  98.0  89.0  95.0  90.0  80.0
+# 재호  20.0  30.0  40.0  73.0  92.0
+# 수지  85.0  74.0  25.0  79.0  98.0
+# 초아  94.0  25.0  85.0  88.0  78.0
+# 용식   0   0   0   0   0
+# 근정   0   0   0   0   0 --- > 없는 이름은 0으로 다 채워라
+
+ndf = ndf.reset_index()
+print(ndf)
+#    이름  수학  영어  도덕  역사  과학
+# 0  설아  98  89  95  90  80
+# 1  재호  20  30  40  73  92
+# 2  수지  85  74  25  79  98
+# 3  초아  94  25  85  88  78
+# 4  용식   0   0   0   0   0
+# 5  근정   0   0   0   0   0
+print(ndf.sort_index(ascending=False))
+#    이름  수학  영어  도덕  역사  과학
+# 5  근정   0   0   0   0   0
+# 4  용식   0   0   0   0   0
+# 3  초아  94  25  85  88  78
+# 2  수지  85  74  25  79  98
+# 1  재호  20  30  40  73  92
+# 0  설아  98  89  95  90  80
+print(ndf.sort_values('역사', ascending=False))
+#    이름  수학  영어  도덕  역사  과학
+# 0  설아  98  89  95  90  80
+# 3  초아  94  25  85  88  78
+# 2  수지  85  74  25  79  98
+# 1  재호  20  30  40  73  92
+# 4  용식   0   0   0   0   0
+# 5  근정   0   0   0   0   0
+
+# 산술연산
+student = pd.Series({'국어': 100, "수학": 200, "영어": 300})
+per = student/100
+print(student)
+print(per)
+# 국어    100
+# 수학    200
+# 영어    300
+# dtype: int64
+# 국어    1.0
+# 수학    2.0
+# 영어    3.0
+# dtype: float64
+
+student1 = pd.Series({'국어': 100, "수학": 20, "영어": 30})
+student2 = pd.Series({'국어': 60, "수학": 70, "영어": 50})
+add = student1 + student2
+print(add)
